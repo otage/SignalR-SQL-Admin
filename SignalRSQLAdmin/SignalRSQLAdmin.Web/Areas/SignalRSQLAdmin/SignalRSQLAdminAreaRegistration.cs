450462f50826eaa7701793858e60ae2c10aba44d
otage <ameunier@intechinfo.fr>
2014-09-09 16:13:41 +0200

    configured some shit

---
+++ SignalRSQLAdmin/SignalRSQLAdmin.Web/Areas/SignalRSQLAdmin/SignalRSQLAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SignalRSQLAdmin.Web.Areas.SignalRSQLAdmin
+{
+    public class SignalRSQLAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SignalRSQLAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SignalRSQLAdmin_default",
+                "SignalRSQLAdmin/{controller}/{action}/{id}",
+                new { controller = "Main", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
