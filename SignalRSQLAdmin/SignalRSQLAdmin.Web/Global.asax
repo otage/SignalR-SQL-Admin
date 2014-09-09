37e80619313c4c2509a6b9e8816cb038f691d8a1
otage <ameunier@intechinfo.fr>
2014-09-09 12:36:20 +0200

    First Commit Bitches

---
+++ SignalRSQLAdmin/SignalRSQLAdmin.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRSQLAdmin.Web.MvcApplication" Language="C#" %>
---
+++ SignalRSQLAdmin/SignalRSQLAdmin.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRSQLAdmin.Web.Startup))]
+namespace SignalRSQLAdmin.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
