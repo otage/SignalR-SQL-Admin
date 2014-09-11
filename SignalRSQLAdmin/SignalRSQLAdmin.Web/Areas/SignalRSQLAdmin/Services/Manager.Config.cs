196f7b2e1db5ee764c6f43b4cec4954cc3c662a6
otage <ameunier@intechinfo.fr>
2014-09-11 17:26:55 +0200

    Connection

--- SignalRSQLAdmin/SignalRSQLAdmin.Web/Areas/SignalRSQLAdmin/Services/Manager.Config.cs
+++ SignalRSQLAdmin/SignalRSQLAdmin.Web/Areas/SignalRSQLAdmin/Services/Manager.Config.cs
@@ -7,5 +7,15 @@ namespace SignalRSQLAdmin.Web.Areas.SignalRSQLAdmin.Services
 {
     public abstract partial class Manager
     {
+        string _server = @".\SQLEXPRESS";
+        string _serverUserId = "sa";
+        string _serverPassword = "vii2s8di";
+
+        protected bool IsTrustedConnection = true;
+
+        private string GetConnectionString()
+        {
+            return String.Format(@"Server={0};Database={1};Trusted_Connection=True;", _server, _dbName);
+        }
     }
 }
